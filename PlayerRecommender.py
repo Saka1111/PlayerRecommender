0a0dfd682e7027379d429ed7088c119085378f94
Saka Samad <sakasamad99@gmail.com>
2026-01-29 14:19:39 +0000

    Update PlayerRecommender.py

--- PlayerRecommender.py
+++ PlayerRecommender.py
@@ -1,11 +1,5 @@
 import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 
-from sklearn.cluster import KMeans
-from sklearn.mixture import GaussianMixture
-from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import MinMaxScaler
 
 import streamlit as st
